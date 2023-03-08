7d3e378a6ecdd95c089380f87b967e81c44517f2
Hector DelaRosa <hdelaros@gmail.com>
2023-03-07 22:35:58 -0500

    Start timer work/break functionality, title_label styling

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -8,14 +9,44 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+# ---------------------------- TIMER MECHANISM ------------------------------- #
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+def start_timer():
+    global reps
+    reps += 1
+    if reps == 8:
+        count_down(LONG_BREAK_MIN)
+        title_label.config(text='BREAK')
+    elif reps % 2 == 0:
+        count_down(SHORT_BREAK_MIN)
+        title_label.config(text='BREAK')
+    else:
+        count_down(WORK_MIN)
+        title_label.config(text='WORK')
+
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+
+def count_down(count):
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+
+    if count_sec < 10:
+        count_sec = f'0{count_sec}'
+
+    canvas.itemconfig(canvas_text, text=f'{count_min}:{count_sec}')
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
 
 # ---------------------------- UI SETUP ------------------------------- #
+
 
 window = Tk()
 window.title('Pomodoro')
@@ -31,10 +62,10 @@ check_mark_label.grid(column=1, row=3)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file='tomato.png')
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(103, 130, text='00:00', fill='white', font=(FONT_NAME, 35, 'bold'))
+canvas_text = canvas.create_text(103, 130, text='00:00', fill='white', font=(FONT_NAME, 35, 'bold'))
 canvas.grid(column=1, row=1)
 
-start_button = Button(text='start', highlightbackground=YELLOW)
+start_button = Button(text='start', highlightbackground=YELLOW, command=start_timer)
 start_button.grid(column=0, row=2)
 
 reset_button = Button(text='reset', highlightbackground=YELLOW)
